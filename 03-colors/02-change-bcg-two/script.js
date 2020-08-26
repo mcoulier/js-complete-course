75fd8282c6a7a743e26654adb89df130b9b403bf
Michiel Coulier <michielcoulier@gmail.com>
2020-08-26 14:36:25 +0200

    used mouseEvent

--- 03-colors/02-change-bcg-two/script.js
+++ 03-colors/02-change-bcg-two/script.js
@@ -10,7 +10,8 @@
 // You will have time to focus on it later.
 
 (function() {
-
-    // your code here
-
+    document.querySelector('button').onclick  = function (event) {
+        let color = document.getElementById("color").value;
+        document.body.style.backgroundColor = color;
+    }
 })();
