31068f0978bbe3d7e92031618a7f06e5f00932ee
Michiel Coulier <michielcoulier@gmail.com>
2020-08-26 10:06:31 +0200

    adding factorial

--- 02-maths/05-factorial/script.js
+++ 02-maths/05-factorial/script.js
@@ -9,14 +9,19 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+
 (function() {
-    
+
     // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("run").addEventListener("click", function() {
-
-        // your code here
-
+        var i, no, fact;
+        fact=1;
+        no= +document.getElementById("number").value;
+        for(i=1; i<=no; i++)
+        {
+            fact= fact*i;
+        }
+        document.getElementById("number").value= fact;
     });
-
 })();
