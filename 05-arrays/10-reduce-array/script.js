7010f0e3d596246d9535fce2799e97fba95ef9b4
Michiel Coulier <michielcoulier@gmail.com>
2020-08-27 16:47:08 +0200

    finished reduce array

--- 05-arrays/10-reduce-array/script.js
+++ 05-arrays/10-reduce-array/script.js
@@ -10,85 +10,93 @@
 // You will have time to focus on it later.
 
 (function() {
+    document.getElementById('run').addEventListener('click', function (e) {
 
-    var people = [
-        {
-            firstname: "Grenville",
-            lastname: "Stive",
-            age: 64,
-        },
-        {
-            firstname: "Luis",
-            lastname: "Jedrzejewsky",
-            age: 73,
-        },
-        {
-            firstname: "Teodor",
-            lastname: "Warrington",
-            age: 56,
-        },
-        {
-            firstname: "Danya",
-            lastname: "Bigby",
-            age: 17,
-        },
-        {
-            firstname: "Jemmy",
-            lastname: "Goodbur",
-            age: 87,
-        },
-        {
-            firstname: "Vilhelmina",
-            lastname: "Fost",
-            age: 43,
-        },
-        {
-            firstname: "Igor",
-            lastname: "Putson",
-            age: 45,
-        },
-        {
-            firstname: "Klement",
-            lastname: "Braybrook",
-            age: 88,
-        },
-        {
-            firstname: "Trefor",
-            lastname: "Le Jean",
-            age: 69,
-        },
-        {
-            firstname: "Tarrah",
-            lastname: "de Clerc",
-            age: 77,
-        },
-        {
-            firstname: "Brad",
-            lastname: "Hookes",
-            age: 82,
-        },
-        {
-            firstname: "Kippar",
-            lastname: "Fancet",
-            age: 25,
-        },
-        {
-            firstname: "Jecho",
-            lastname: "Hawket",
-            age: 87,
-        },
-        {
-            firstname: "Lief",
-            lastname: "Blazy",
-            age: 80,
-        },
-        {
-            firstname: "Jo-ann",
-            lastname: "Sacase",
-            age: 81,
-        },
-    ];
-
-    // your code here
+        var people = [
+            {
+                firstname: "Grenville",
+                lastname: "Stive",
+                age: 64,
+            },
+            {
+                firstname: "Luis",
+                lastname: "Jedrzejewsky",
+                age: 73,
+            },
+            {
+                firstname: "Teodor",
+                lastname: "Warrington",
+                age: 56,
+            },
+            {
+                firstname: "Danya",
+                lastname: "Bigby",
+                age: 17,
+            },
+            {
+                firstname: "Jemmy",
+                lastname: "Goodbur",
+                age: 87,
+            },
+            {
+                firstname: "Vilhelmina",
+                lastname: "Fost",
+                age: 43,
+            },
+            {
+                firstname: "Igor",
+                lastname: "Putson",
+                age: 45,
+            },
+            {
+                firstname: "Klement",
+                lastname: "Braybrook",
+                age: 88,
+            },
+            {
+                firstname: "Trefor",
+                lastname: "Le Jean",
+                age: 69,
+            },
+            {
+                firstname: "Tarrah",
+                lastname: "de Clerc",
+                age: 77,
+            },
+            {
+                firstname: "Brad",
+                lastname: "Hookes",
+                age: 82,
+            },
+            {
+                firstname: "Kippar",
+                lastname: "Fancet",
+                age: 25,
+            },
+            {
+                firstname: "Jecho",
+                lastname: "Hawket",
+                age: 87,
+            },
+            {
+                firstname: "Lief",
+                lastname: "Blazy",
+                age: 80,
+            },
+            {
+                firstname: "Jo-ann",
+                lastname: "Sacase",
+                age: 81,
+            },
+        ];
+        Array.prototype.sum = function (prop) {
+            var total = 0
+            for ( var i = 0, _len = this.length; i < _len; i++ ) {
+                total += this[i][prop]
+            }
+            return total
+        }
+        console.log(people.sum('age'));
+    })
 
 })();
