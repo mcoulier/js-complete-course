6d5112bba6e8024260d69a6d979cc700b7d9ee6f
Michiel Coulier <michielcoulier@gmail.com>
2020-08-27 09:24:31 +0200

    added code

--- 04-dates/01-title-by-hour-one/script.js
+++ 04-dates/01-title-by-hour-one/script.js
@@ -11,8 +11,12 @@
 
 (function() {
 
-    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
+    var time = new Date().getHours();
 
-    // your code here
-
+    if (time < 18) {
+        document.getElementById("target").innerHTML = "Hello";
+    }
+    if (time > 18) {
+        document.getElementById("target").innerHTML = "Good Evening";
+    }
 })();
