e3a1a2533eab1e2b5a57e190966b64d6e10d3bfa
Michiel Coulier <michielcoulier@gmail.com>
2020-08-28 15:17:34 +0200

    finished carousel

--- 06-dom/06-mini-carousel/script.js
+++ 06-dom/06-mini-carousel/script.js
@@ -10,15 +10,35 @@
 // You will have time to focus on it later.
 
 (function() {
+        var gallery = [
+            "../../_shared/img/bell.svg",
+            "../../_shared/img/clock.svg",
+            "../../_shared/img/compass.svg",
+            "../../_shared/img/lemon.svg",
+            "../../_shared/img/map.svg",
+        ];
+var counter = 1;
 
-    var gallery= [
-        "../../_shared/img/bell.svg",
-        "../../_shared/img/clock.svg",
-        "../../_shared/img/compass.svg",
-        "../../_shared/img/lemon.svg",
-        "../../_shared/img/map.svg",
-    ];
-
-    // your code here
-
+    next.onclick = function () {
+        if (counter == 1) {
+            document.querySelector("img").src = gallery[1];
+            counter++;
+        }
+        else if (counter == 2) {
+            document.querySelector("img").src = gallery[2];
+            counter++;
+        }
+        else if (counter == 3) {
+            document.querySelector("img").src = gallery[3];
+            counter++;
+        }
+        else if (counter == 4) {
+            document.querySelector("img").src = gallery[4];
+            counter++;
+        }
+        else if (counter == 5) {
+            document.querySelector("img").src = gallery[0];
+            counter = 1;
+        }
+    }
 })();
