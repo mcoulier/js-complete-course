d6f67698781772203f3ff6c548796edb71ed6e57
Michiel Coulier <michielcoulier@gmail.com>
2020-09-01 13:52:16 +0200

    finished change event 2

--- 06-dom/12-change-event-input-two/script.js
+++ 06-dom/12-change-event-input-two/script.js
@@ -10,7 +10,11 @@
 // You will have time to focus on it later.
 
 (function() {
-
-    // your code here
-
+document.getElementById("pass-one").onkeypress = function (){
+    var pasw = document.getElementById("pass-one").value;
+    var pasreq = /^(?=.*\d){2,}.{8,}$/;
+    if (pasw.match(pasreq)) {
+        document.getElementById("validity").innerHTML = "Ok";
+    }
+    }
 })();
