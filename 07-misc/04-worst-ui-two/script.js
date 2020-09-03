a197ce9710cee6321638f92abb3b0847fde73479
Michiel Coulier <michielcoulier@gmail.com>
2020-09-03 15:06:20 +0200

    finished worst ui 2

--- 07-misc/04-worst-ui-two/script.js
+++ 07-misc/04-worst-ui-two/script.js
@@ -9,8 +9,50 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    // your code here
+    var partOne = 460;
+    var partTwo = 0;
+    var partThree = 0;
+    var partFour = 0;
+    var a = "0" + partTwo;
+    var b = "0" + partThree;
+    var c = "0" + partFour;
+    target.innerHTML = "0" + partOne + a + b + c;
+
+    document.getElementById('part-one').onclick = function () {
+        if (partOne<499) {partOne++};
+        target.innerHTML = "0" + partOne + a + b + c;
+    }
+    document.getElementById('part-two').onclick = function () {
+        if (partTwo<99) {partTwo++};
+        if (partTwo < 10) {
+            a = "0" + partTwo
+        } else {
+            a = partTwo
+        }
+        ;
+        target.innerHTML = "0" + partOne + a + b + c;
+    }
+    document.getElementById('part-three').onclick = function () {
+        if (partThree<99) {partThree++};
+        if (partThree < 10) {
+            b = "0" + partThree
+        } else {
+            b = partThree
+        }
+        ;
+        target.innerHTML = "0" + partOne + a + b + c;
+    }
+    document.getElementById('part-four').onclick = function () {
+        if (partFour<99) {partFour++};
+        if (partFour < 10) {
+            c = "0" + partFour
+        } else {
+            c = partFour
+        }
+        ;
+        target.innerHTML = "0" + partOne + a + b + c;
+    }
 
 })();
