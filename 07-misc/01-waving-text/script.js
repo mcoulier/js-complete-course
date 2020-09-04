13231b483de889863fed7fbedd091d9edcff0014
Michiel Coulier <michielcoulier@gmail.com>
2020-09-04 12:06:55 +0200

    Finished with help

--- 07-misc/01-waving-text/script.js
+++ 07-misc/01-waving-text/script.js
@@ -9,8 +9,19 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+
 (function() {
-
-    // your code here
-
+    let txt = document.getElementById("target").innerText;
+    let chars = Array.from(txt);
+    let wave = "";
+    let fsize = [1, 2, 3, 4, 5, 4, 3, 2, 1,]
+    for (let i = 0; i < chars.length; i++){
+        wave += "<span>" + chars[i] + "</span>";
+    }
+    document.getElementById("target").innerHTML = wave;
+    let characters = document.getElementsByTagName("span");
+    for (let x = 0; x < characters.length; x++){
+        let y = x+1;
+        characters[y].style.fontSize = fsize[x % fsize.length] + "1px";
+    }
 })();
