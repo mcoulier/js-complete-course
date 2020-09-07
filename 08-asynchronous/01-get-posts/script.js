064736cac8f70e7f242a41ed833d1fe9daf9143a
Michiel Coulier <michielcoulier@gmail.com>
2020-09-07 10:22:24 +0200

    finished get posts

--- 08-asynchronous/01-get-posts/script.js
+++ 08-asynchronous/01-get-posts/script.js
@@ -10,5 +10,14 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    document.getElementById("run").addEventListener("click", ()=> {
+        window.lib.getPosts((error, array)=> {
+            if (error) {
+                console.log(1);
+            } else {
+                console.log(null);
+                console.log(array);
+            }
+        })
+    })
 })();
