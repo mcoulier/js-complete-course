8e06ca5d8d28c950847f58e3e02507985d0e1c27
Michiel Coulier <michielcoulier@gmail.com>
2020-09-08 16:51:37 +0200

    End of day commit

--- 09-fetch/02-list-to-template/script.js
+++ 09-fetch/02-list-to-template/script.js
@@ -14,6 +14,14 @@
     run.addEventListener("click", fetchHeroes)
 
     function fetchHeroes(){
+        let temp, item, a, i;
+        temp = document.getElementsByTagName("template")[0];
+        item = temp.content.querySelector("target");
+        for (i = 0; i < 5; i++){
+            a = document.importNode(item, true);
+            a.textContent +=
+            document.body.appendChild(a);
+        }
         fetch('http://localhost:3000/heroes')
             .then(response => response.json())
             .then(data => {
