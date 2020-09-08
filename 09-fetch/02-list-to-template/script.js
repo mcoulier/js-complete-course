1e86dc7f7d1f1af1333d87e1a620a14ac013e0d4
Michiel Coulier <michielcoulier@gmail.com>
2020-09-08 14:39:41 +0200

    Added basic structure

--- 09-fetch/02-list-to-template/script.js
+++ 09-fetch/02-list-to-template/script.js
@@ -10,5 +10,15 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    let run = document.getElementById("run")
+    run.addEventListener("click", fetchHeroes)
+
+    function fetchHeroes(){
+        fetch('http://localhost:3000/heroes')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+            })
+            .catch(error => console.error(error))
+    }
 })();
