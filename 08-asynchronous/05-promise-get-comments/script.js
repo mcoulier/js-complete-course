d4dbff79fdb63b915a74c24bc418d447ca9d7e0f
Michiel Coulier <michielcoulier@gmail.com>
2020-09-08 10:20:58 +0200

    Array shows, still need to add comments

--- 08-asynchronous/05-promise-get-comments/script.js
+++ 08-asynchronous/05-promise-get-comments/script.js
@@ -11,16 +11,21 @@
 
 (() => {
     let run = document.getElementById("run")
-    run.addEventListener("click", getPostsWithComments)
+    run.addEventListener("click", getPostsConsole)
 
-    function getPostsWithComments(){
-        window.lib.getPosts(function(error, articles){
-            articles.forEach(post => {
-                window.lib.getComments(articles.id, function(error, comments) {
-                    articles.comment = comments;
-                    console.log(post);
-                })
-            })
+    function getPostsConsole(){
+        return new Promise((resolve, reject) => {
+            resolve(window.lib.getPosts())
         })
+            .then((result) => result.forEach(function (article) {
+                    new Promise((resolve, reject) => {
+                        let result = window.lib.getComments(article.id)
+                        resolve(result)
+                    }).then(result => console.log(result))
+            }
+            )).catch(function (error) {
+                console.log(error)
+            })
     }
 })();
+
