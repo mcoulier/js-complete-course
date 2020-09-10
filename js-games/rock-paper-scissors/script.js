39549fb2abad088d87bfc38bc84bf405e1d5f5e1
Michiel Coulier <michielcoulier@gmail.com>
2020-09-10 15:56:46 +0200

    working in console, now trying html

--- js-games/rock-paper-scissors/script.js
+++ js-games/rock-paper-scissors/script.js
@@ -12,5 +12,24 @@ if (computerChoice < 0.34){
 const compare = function (choice1, choice2){
     if(choice1 === choice2){
         return "Tie";
+    } else if (choice1 === "rock"){
+        if (choice2 === "scissors"){
+            return "Rock wins"
+        } else if (choice2 === "paper"){
+            return "Paper wins";
+        }
+    } else if (choice1 === "paper"){
+        if (choice2 === "scissors"){
+            return "Scissors win"
+        } else if (choice2 === "rock"){
+            return "Paper wins";
+        }
+    } else if (choice1 === "scissors"){
+        if (choice2 === "rock"){
+            return "Rock wins"
+        } else if (choice2 === "paper"){
+            return "Scissors win";
+        }
     }
 }
+console.log(compare(userChoice,computerChoice))
