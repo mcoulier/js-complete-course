4d72cb213fcef117299fdbe42721bb4b2dc215d7
Michiel Coulier <michielcoulier@gmail.com>
2020-09-10 11:45:48 +0200

    shuffle & createdeck

--- js-games/21/script.js
+++ js-games/21/script.js
@@ -1,12 +1,27 @@
-const suits = ["spades", "diamonds", "clubs", "hearts"];
-const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+let suits = ["spades", "diamonds", "clubs", "hearts"];
+let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
-function getDeck(){
-    const deck = new Array();
-
+function createDeck() {
+    let deck = []
     for(let i = 0; i < suits.length; i++){
-        for(let x = 0; x < values.length; x++){
-
+        for(let x = 0; x < values.length; x++) {
+            let card = {
+                suit: suits[i],
+                value: values[x]
+            };
+        deck.push(card);
         }
+    }
+    return deck;
+}
+
+function shuffle(deck) {
+    for (let i = 0; i < 1000; i++){
+        const location1 = Math.floor((Math.random() * deck.length));
+        const location2 = Math.floor((Math.random() * deck.length));
+        const tmp = deck[location1];
+
+        deck[location1] = deck[location2];
+        deck[location2] = tmp;
     }
 }
