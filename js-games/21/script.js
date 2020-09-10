ab799d7f76f7f5ac896f51eddff2f7b8d76c0a63
Michiel Coulier <michielcoulier@gmail.com>
2020-09-10 10:03:52 +0200

    starting with building card deck

---
+++ js-games/21/script.js
@@ -0,0 +1,12 @@
+const suits = ["spades", "diamonds", "clubs", "hearts"];
+const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+function getDeck(){
+    const deck = new Array();
+
+    for(let i = 0; i < suits.length; i++){
+        for(let x = 0; x < values.length; x++){
+
+        }
+    }
+}
