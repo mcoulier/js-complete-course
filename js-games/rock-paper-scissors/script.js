8c05d284ceaf8c91101c7432f41c347792678996
Michiel Coulier <michielcoulier@gmail.com>
2020-09-11 11:18:07 +0200

    deleted onclick html & added function

--- js-games/rock-paper-scissors/script.js
+++ js-games/rock-paper-scissors/script.js
@@ -1,53 +1,54 @@
-
-
-document.getElementById("rock").addEventListener("click", ()=>{
-    userChoice = "rock";
-    playGame();
-})
-
-document.getElementById("paper").addEventListener("click", ()=>{
-    userChoice = "paper";
-    playGame();
-})
-
-document.getElementById("scissors").addEventListener("click", ()=>{
-    userChoice = "scissors";
-    playGame();
-})
-
-function playGame(){
+(()=>{
 
     let userChoice;
-    let computerChoice = Math.random();
 
-    if (computerChoice < 0.34){
-        computerChoice = "rock";
-    } else if(computerChoice <= 0.67 ){
-        computerChoice = "paper";
-    } else {
-        computerChoice = "scissors";
-    }
+    document.getElementById("rock").addEventListener("click", () => {
+        userChoice = "rock";
+        playGame();
+    })
 
-    if(userChoice === computerChoice){
-        return "Tie";
-    } else if (userChoice === "rock"){
-        if (computerChoice === "scissors"){
-            return "Rock wins"
-        } else if (computerChoice === "paper"){
-            return "Paper wins";
+    document.getElementById("paper").addEventListener("click", () => {
+        userChoice = "paper";
+        playGame();
+    })
+
+    document.getElementById("scissors").addEventListener("click", () => {
+        userChoice = "scissors";
+        playGame();
+    })
+
+    function playGame() {
+        let computerChoice = Math.random();
+
+        if (computerChoice < 0.34) {
+            computerChoice = "rock";
+        } else if (computerChoice <= 0.67) {
+            computerChoice = "paper";
+        } else {
+            computerChoice = "scissors";
         }
-    } else if (userChoice === "paper"){
-        if (computerChoice === "scissors"){
-            return "Scissors win"
-        } else if (computerChoice === "rock"){
-            return "Paper wins";
-        }
-    } else if (userChoice === "scissors"){
-        if (computerChoice === "rock"){
-            return "Rock wins"
-        } else if (computerChoice === "paper"){
-            return "Scissors win";
+
+        if (userChoice === computerChoice) {
+            return "Tie";
+        } else if (userChoice === "rock") {
+            if (computerChoice === "scissors") {
+                return "Rock wins"
+            } else if (computerChoice === "paper") {
+                return "Paper wins";
+            }
+        } else if (userChoice === "paper") {
+            if (computerChoice === "scissors") {
+                return "Scissors win"
+            } else if (computerChoice === "rock") {
+                return "Paper wins";
+            }
+        } else if (userChoice === "scissors") {
+            if (computerChoice === "rock") {
+                return "Rock wins"
+            } else if (computerChoice === "paper") {
+                return "Scissors win";
+            }
         }
     }
-    console.log(userChoice,computerChoice)
-}
+    document.getElementById("outcome")
+})();
