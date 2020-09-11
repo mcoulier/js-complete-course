53879523de72d36c724cb604aea418f8e4c03338
Michiel Coulier <michielcoulier@gmail.com>
2020-09-11 12:26:33 +0200

    Added bg color & edit console comment

--- js-games/rock-paper-scissors/script.js
+++ js-games/rock-paper-scissors/script.js
@@ -22,6 +22,12 @@
         var playerScore = 0;
         var computerScore = 0;
 
+        if (result === "Player wins"){
+            playerScore++
+        } else if (result === "Computer wins"){
+            computerScore++
+        }
+
         let computerChoice = Math.random();
 
         if (computerChoice < 0.34) {
@@ -31,30 +37,39 @@
         } else {
             computerChoice = "scissors";
         }
+
+
         // checking answers
-        console.log(userChoice,computerChoice)
+        console.log("Player: " + userChoice,"Computer: " + computerChoice)
 
         if (userChoice === computerChoice) {
             result = "tie";
         } else if (userChoice === "rock") {
             if (computerChoice === "scissors") {
-                result = "Player wins"
+                result = "Player wins";
+                playerScore++;
             } else if (computerChoice === "paper") {
                 result = "Computer wins";
+                computerScore++;
             }
         } else if (userChoice === "paper") {
             if (computerChoice === "scissors") {
-                result = "Computer wins"
+                result = "Computer wins";
+                computerScore++;
             } else if (computerChoice === "rock") {
                 result = "Player wins";
+                playerScore++;
             }
         } else if (userChoice === "scissors") {
             if (computerChoice === "rock") {
-                result = "Computer wins"
+                result = "Computer wins";
+                computerScore++;
             } else if (computerChoice === "paper") {
                 result = "Player wins";
+                playerScore++;
             }
         }
         document.getElementById("outcome").value = result;
+        document.getElementById("score").value = playerScore + " - " + computerScore;
     }
 })();
